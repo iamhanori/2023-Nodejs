065f20f83c75cb02b7644ba94613ed058ef315fb
하늘 김 <s2122@e-mirim.hs.kr>
2023-03-10 10:14:11 +0900

    JSON parse, stringify

---
+++ ex04.js
@@ -0,0 +1,10 @@
+const json = '{"result":true, "count":42 }';
+const obj = JSON.parse(json);
+
+console.log(obj.count);
+// expected output: 42
+console.log(obj.result);
+// expected output: true
+
+console.log(JSON.stringify({ x:5, y:6}));
+// expected output: "{ "x":5, "y":6}"
