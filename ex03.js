4e9af62d693e411bca25a66f1cbbaf523e2e7009
하늘 김 <s2122@e-mirim.hs.kr>
2023-03-10 10:00:44 +0900

    Object

---
+++ ex03.js
@@ -0,0 +1,12 @@
+const cathy = {
+    "name" : "cathy",
+    "age" : 19,
+    "skills" : ["js", "python", "cobol"]
+};
+
+console.log(cathy); // { name: 'cathy', age: 19, skills: [ 'js', 'python', 'cobol' ] }
+// cathy
+console.log(cathy.name);
+console.log(cathy.age);
+console.log(cathy.skills);
+console.log(cathy["name"]);
