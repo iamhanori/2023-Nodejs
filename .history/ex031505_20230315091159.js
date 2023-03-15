64144af47275902b03cd0112eaac3a93b74450ce
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-15 09:12:49 +0900

    구조분해 할당

---
+++ .history/ex031505_20230315091159.js
@@ -0,0 +1,27 @@
+// 구조분해 할당
+
+const example = {a:123, b:{c:11, d:16}};
+
+// case1:
+/*
+const a = example.a;
+const b = example.b;
+*/
+
+// case2:
+const {a, b:{d}} = example;
+
+arr = [1, 2, 3, 4, 5];
+
+// case1:
+/*
+const x = arr[0];
+const y = arr[2];
+const z = arr[4];
+*/
+
+// case2:
+const [x,,y,,z] = arr;
+
+console.log(a, d);
+console.log(x, y, z);
